e00137ae5bd5220fd1a8e6a279bbc4473d38de1b
Patryk Wasiewicz <pat.wasiewicz@gmail.com>
2014-12-04 22:21:14 +0100

    Project for compressing, added Ionic.zip

---
+++ NeuSim/Helpers/PathCompressor.cs
@@ -0,0 +1,33 @@
+﻿namespace NeuSim.Helpers
+{
+    using System.Collections.Generic;
+    using System.IO.Compression;
+
+    internal class PathCompressor
+    {
+        private readonly string path;
+
+        private readonly string name;
+
+        private readonly string savePath;
+
+        private readonly HashSet<string> ignoredExtensions;
+
+        public PathCompressor(string path, string name, string savePath)
+        {
+            this.path = path;
+            this.name = name;
+            this.savePath = savePath;
+
+            this.ignoredExtensions = new HashSet<string>();
+        }
+
+        public void IgnoreExts(params string[] exts)
+        {
+            foreach (var ext in exts)
+            {
+                this.ignoredExtensions.Add(ext);
+            }
+        }
+    }
+}
